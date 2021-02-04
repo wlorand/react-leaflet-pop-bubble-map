1e3ce3526f3be04543e92430d4afa4f8fbd25a40
Will Lorand <will@milemarker3.org>
2021-02-03 20:53:34 -0800

    dev complete on bubble map

--- src/components/maps/AsianPopulationMap.js
+++ src/components/maps/AsianPopulationMap.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
-// import { MapContainer, TileLayer, CircleMarker } from 'react-leaflet';
+// React-Leaflet
+import { MapContainer, TileLayer, CircleMarker, Tooltip } from 'react-leaflet';
 
-// leaflet styles
+// data
+import { data } from '../../data/asian_city_populations';
+
+// leaflet styles (from node_modules)
+import 'leaflet/dist/leaflet.css';
+// local styles
+import '../../styles/asian-population-map.css';
 
 function AsianPopulationMap() {
-  return <div>map</div>;
+  const tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+  return (
+    <div className="map-container">
+      <MapContainer center={[25, 85]} zoom={3.45}>
+        <TileLayer url={tileUrl} />
+        {data.cities.map((city, i) => (
+          <CircleMarker
+            center={[city['coordinates'][1], city['coordinates'][0]]}
+            radius={20 * Math.log(city['population'] / 10000000)}
+            key={i}
+            fillOpacity={0.55}
+            stroke={false}
+          >
+            <Tooltip direction="right" offset={[-8, -2]} opacity={0.7}>
+              <span>{`${city['name']}: Population ${city['population']}`}</span>
+            </Tooltip>
+          </CircleMarker>
+        ))}
+      </MapContainer>
+    </div>
+  );
 }
 
 export default AsianPopulationMap;
